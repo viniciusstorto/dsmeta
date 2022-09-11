3ab1d66e9eae5635b319a9a6f38781bc0ee82555
Vinícius Storto <viniciusstorto@hotmail.com>
2022-09-11 19:03:07 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import NotificationButton from "./components/NotificationButton"
+
+
 function App() {
   return(
-    <h1>Olá mundo</h1>
-  )
-}
+      <>
+       <h1>Olá mundo</h1>
+       <NotificationButton />
+       <NotificationButton />
+       <NotificationButton />
+      </>
+     
+      )
+}   
 
 export default App
