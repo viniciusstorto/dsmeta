ffe26bd15848f7af6810183fd8bf5a7693ebd1cf
Vinícius Storto <viniciusstorto@hotmail.com>
2022-09-18 13:40:48 -0300

    other componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,22 @@
 import NotificationButton from "./components/NotificationButton"
 import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 
 function App() {
-  return(
-      <>
-       <Header />
-       <NotificationButton />
-       <NotificationButton />
-       <NotificationButton />
-      </>
-     
-      )
-}   
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
+
+  )
+}
 
 export default App
